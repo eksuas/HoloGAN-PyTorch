682908710a6009a428631fe8293fb629277a38e3
eksuas <e181920@metu.edu.tr>
2020-05-13 18:17:43 +0300

    spectural normalization eklendi

--- utils.py
+++ utils.py
@@ -25,33 +25,3 @@ def load_dataset(args):
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,\
                     shuffle=True, **kwargs)
     return train_loader
-
-
-def spectral_norm(w, iteration=1, u_weight=None):
-    w_shape = list(w.shape)
-    w = w.reshape(-1, w_shape[-1])
-    if u_weight is None:
-        u = tf.get_variable("u", [1, w_shape[-1]], initializer=tf.truncated_normal_initializer(), trainable=False)
-    else:
-        u = u_weight
-
-    u_hat = u
-    v_hat = None
-    for i in range(iteration):
-        """
-        power iteration
-        Usually iteration = 1 will be enough
-        """
-        v_ = tf.matmul(u_hat, tf.transpose(w))
-        v_hat = l2_norm(v_)
-
-        u_ = tf.matmul(v_hat, w)
-        u_hat = l2_norm(u_)
-
-    sigma = tf.matmul(tf.matmul(v_hat, w), tf.transpose(u_hat))
-    w_norm = w / sigma
-
-    with tf.control_dependencies([u.assign(u_hat)]):
-        w_norm = tf.reshape(w_norm, w_shape)
-
-    return w_norm
