92b04d263b4853731355215266ae44a31eae883b
GOKHAN OZSARI <gozsari@gmail.com>
2020-05-24 17:29:45 +0300

    Update and rename download_extract_data.sh to download_extract_dataset.sh

--- download_extract_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Downloading..."
-wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1JKAluJEagidnUYin77yjoiN_FW63zuZj' -O img_align_celeba.zip
-echo "Download completed."
-echo "Extracting..."
-unzip img_align_celeba.zip -d ../dataset/celebA
-echo "Extraction completed."
