6574ce0360c06e15dd14e24b66df67c3828382cc
GOKHAN OZSARI <gozsari@gmail.com>
2020-05-24 17:30:42 +0300

    Update download_extract_dataset.sh

--- download_extract_dataset.sh
+++ download_extract_dataset.sh
@@ -3,7 +3,7 @@ if [ -f img_align_celeba.zip ]; then
 	echo "Aldready downloaded!"
 else
 	echo "Downloading"
-	wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1ISR9DSV4UNxZv_UPWhtETWBPkol_Ufj2' -O img_align_celeba.zip
+	wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1JKAluJEagidnUYin77yjoiN_FW63zuZj' -O img_align_celeba.zip
 	echo "Download completed!"
 fi
 PATH="../dataset/celebA"
