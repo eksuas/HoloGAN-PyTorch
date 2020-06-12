fix 3d rotation for azimuth

generator.py
@@ -103,7 +103,6 @@ class Generator(nn.Module):
     def transformation3d(self, voxel_array, view_params, size=16, new_size=16):
         # TODO: daha efficient olması için ileri de tek bir matrix formatında oluşturulabilir
 
-        # Rotation Y matrix
         theta = Variable(torch.as_tensor(view_params[:, 0].reshape(-1, 1, 1)).float(),
                          requires_grad=True).to(self.device)
         gamma = Variable(torch.as_tensor(view_params[:, 1].reshape(-1, 1, 1)).float(),
@@ -111,18 +110,19 @@ class Generator(nn.Module):
         ones  = torch.ones(theta.shape, requires_grad=True).to(self.device)
         zeros = torch.zeros(theta.shape, requires_grad=True).to(self.device)
 
-        rot_y = torch.cat([
-            torch.cat([theta.cos(),  zeros,  -theta.sin(),  zeros], dim=2),
-            torch.cat([zeros,        ones,   zeros,         zeros], dim=2),
-            torch.cat([theta.sin(),  zeros,  theta.cos(),   zeros], dim=2),
-            torch.cat([zeros,        zeros,  zeros,         ones],  dim=2)], dim=1)
-
-        # Rotation Z matrix
+        # Rotation azimuth (i.e. rotate around-z)
         rot_z = torch.cat([
-            torch.cat([gamma.cos(),  gamma.sin(),   zeros,  zeros], dim=2),
-            torch.cat([-gamma.sin(), gamma.cos(),   zeros,  zeros], dim=2),
-            torch.cat([zeros,        zeros,         ones,   zeros], dim=2),
-            torch.cat([zeros,        zeros,         zeros,  ones],  dim=2)], dim=1)
+            torch.cat([theta.cos(),   theta.sin(),  zeros,  zeros], dim=2),
+            torch.cat([-theta.sin(),  theta.cos(),  zeros,  zeros], dim=2),
+            torch.cat([zeros,         zeros,        ones,   zeros], dim=2),
+            torch.cat([zeros,         zeros,        zeros,  ones],  dim=2)], dim=1)
+
+        # Rotation elevation (i.e. rotate around-x)
+        rot_y = torch.cat([
+            torch.cat([gamma.cos(),   zeros,  gamma.sin(),  zeros], dim=2),
+            torch.cat([zeros,         ones,   zeros,        zeros], dim=2),
+            torch.cat([-gamma.sin(),  zeros,  gamma.cos(),  zeros], dim=2),
+            torch.cat([zeros,         zeros,  zeros,        ones],  dim=2)], dim=1)
 
         rotation_matrix = torch.matmul(rot_z, rot_y)
 

hologan.py
@@ -75,9 +75,9 @@ class HoloGAN():
             os.makedirs(args.models_dir)
 
         # continue to broken training
+        args.start_epoch = 0
         if args.load_dis is None:
             load_model = ""
-            args.start_epoch = 0
             for modelname in listdir(args.models_dir):
                 if isfile(join(args.models_dir, modelname)) and \
                    ("discriminator.v" in modelname or "generator.v" in modelname):
@@ -216,7 +216,7 @@ class HoloGAN():
         """
         z = self.sample_z(args)
         if args.rotate_azimuth:
-            low, high, step = args.azimuth_low, args.azimuth_high, 10
+            low, high, step = args.azimuth_low, args.azimuth_high+1, 5
         elif args.rotate_elevation:
             low, high, step = args.elevation_low, args.elevation_high, 5
         else:
@@ -235,11 +235,11 @@ class HoloGAN():
         for i in range(low, high, step):
             # Apply only azimuth rotation
             if args.rotate_azimuth:
-                view_in = torch.tensor([(i-90)*math.pi/180, 0, 1.0, 0, 0, 0])
+                view_in = torch.tensor([i*math.pi/180, 0, 1.0, 0, 0, 0])
                 view_in = view_in.repeat(args.batch_size, 1)
             # Apply only elevation rotation
             elif args.rotate_elevation:
-                view_in = torch.tensor([270*math.pi/180, (90-i)*math.pi/180, 1.0, 0, 0, 0])
+                view_in = torch.tensor([270*math.pi/180, i*math.pi/180, 1.0, 0, 0, 0])
                 view_in = view_in.repeat(args.batch_size, 1)
             # Apply default transformation
             else:
@@ -295,11 +295,16 @@ class HoloGAN():
         # the azimuth angle (theta) is around y
         theta = np.random.randint(args.azimuth_low, args.azimuth_high,
                                   (args.batch_size)).astype(np.float)
-        theta = (theta - 90.) * math.pi / 180.0
+        theta = theta * math.pi / 180.0
+
         # the elevation angle (gamma) is around x
-        gamma = np.random.randint(args.elevation_low, args.elevation_high,
-                                  (args.batch_size)).astype(np.float)
-        gamma = (90. - gamma) * math.pi / 180.0
+        if args.elevation_low < args.elevation_high:
+            gamma = np.random.randint(args.elevation_low, args.elevation_high,
+                                      (args.batch_size)).astype(np.float)
+            gamma = gamma * math.pi / 180.0
+        else:
+            gamma = np.zeros(args.batch_size).astype(np.float)
+
         scale = float(np.random.uniform(args.scale_low, args.scale_high))
         shift_x = args.transX_low + np.random.random(args.batch_size) * \
                   (args.transX_high - args.transX_low)